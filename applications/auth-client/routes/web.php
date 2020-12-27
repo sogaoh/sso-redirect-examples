f4357c089e04835000e09ee2b4b5e7abee5b91bb
sogaoh <sogaoh@ant-in-giant.work>
2020-12-27 20:01:27 +0900

    composer require laravel/ui php artisan ui vue --auth Auth::routes(['register' => false, 'reset' => false, 'verify' => false]);

--- applications/auth-client/routes/web.php
+++ applications/auth-client/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['register' => false, 'reset' => false, 'verify' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
