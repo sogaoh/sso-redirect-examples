4a15ea997213055b41a120b3a54d23651838e086
sogaoh <sogaoh@ant-in-giant.work>
2020-12-29 14:13:55 +0900

    chore: adjust default auth scene(s)

--- applications/auth-client/resources/views/welcome.blade.php
+++ applications/auth-client/resources/views/welcome.blade.php
@@ -22,23 +22,22 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                @if (Route::has('sso.cognito.login'))
+                    <a href="{{ route('sso.cognito.login') }}" class="text-sm text-gray-700 underline">Cognito</a>
+                @endif
+                @if (Route::has('login'))
                     @auth
                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+{{--                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>--}}
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
                         @endif
                     @endauth
-                </div>
-            @elseif (Route::has('sso.cognito.login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    <a class="nav-link" href="{{ route('sso.cognito.login') }}">{{ __('Cognito') }}</a>
-                </div>
-            @endif
+                @endif
+            </div>
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
--- applications/auth-client/routes/web.php
+++ applications/auth-client/routes/web.php
@@ -16,10 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Auth::routes([
-    'login' => false,
+    //'login' => false,
     'register' => false,
     'reset' => false,
     'confirm' => false,
