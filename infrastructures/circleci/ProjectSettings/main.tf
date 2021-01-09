4b63d7349ef09315b0e005c8e08379a0f0a34a86
sogaoh <sogaoh@ant-in-giant.work>
2021-01-10 01:31:42 +0900

    feat: add circleci project setting
    - refs https://github.com/mrolla/terraform-provider-circleci

---
+++ infrastructures/circleci/ProjectSettings/main.tf
@@ -0,0 +1,29 @@
+provider "circleci" {
+    api_token    = var.circleci_token
+    vcs_type     = "github"
+    organization = var.circleci_organization
+}
+
+resource "circleci_environment_variable" "aws_access_key" {
+    project = var.circleci_project_name
+    name    = "AWS_ACCESS_KEY_ID"
+    value   = var.aws_access_key
+}
+
+resource "circleci_environment_variable" "aws_secret_key" {
+    project = var.circleci_project_name
+    name    = "AWS_SECRET_ACCESS_KEY"
+    value   = var.aws_secret_key
+}
+
+resource "circleci_environment_variable" "aws_region" {
+    project = var.circleci_project_name
+    name    = "AWS_DEFAULT_REGION"
+    value   = var.aws_region
+}
+
+resource "circleci_environment_variable" "aws_account_id" {
+    project = var.circleci_project_name
+    name    = "AWS_ACCOUNT_ID"
+    value   = var.aws_account_id
+}
